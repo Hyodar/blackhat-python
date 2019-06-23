2685a1df016e9a811fdacea09f3d3cd962d05eb5
Hyodar <franco.b.gomes@gmail.com>
2019-06-23 00:00:30 -0300

    Small improvements to TCP proxy

--- tcp_proxy/tcp_proxy.py
+++ tcp_proxy/tcp_proxy.py
@@ -112,20 +112,23 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first, timeou
 # -----------------------------------------------------------------------------
 
 """
-hexdump(src: bytes, length:int)
+hexdump(src: bytes, length: int)
     Dumps the src as hex in a pretty way
     source: http://code.activestate.com/recipes/142812-hex-dumper/ (comments)
 """
 
 def hexdump(src, length=10):
+
+    src = src.decode('utf-8')
+
     result = []
     digits = 4 if isinstance(src, str) else 2
     for i in range(0, len(src), length):
        s = src[i:i+length]
-       hexa = b' '.join(["%0*X" % (digits, ord(x))  for x in s])
-       text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.'  for x in s])
-       result.append( b"%04X   %-*s   %s" % (i, length*(digits + 1), hexa, text) )
-    return b'\n'.join(result)
+       hexa = ' '.join(["%0*X" % (digits, ord(x))  for x in s])
+       text = ''.join([x if 0x20 <= ord(x) < 0x7F else b'.'  for x in s])
+       result.append( "%04X   %-*s   %s" % (i, length*(digits + 1), hexa, text) )
+    return '\n'.join(result)
 
 # -----------------------------------------------------------------------------
 
